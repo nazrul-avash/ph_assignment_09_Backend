1019f837ca8d61e7134bb28cace110ef6031e210
nazrul-avash <nazrul.avash@gmail.com>
2026-05-25 09:19:32 +0600

    bug fixed

--- index.js
+++ index.js
@@ -189,13 +189,6 @@ app.get("/appointments", async (req, res) => {
 
     
 
-    app.delete("/destination/:id", async (req, res) => {
-      const { id } = req.params;
-      const result = await destinationCollection.deleteOne({
-        _id: new ObjectId(id),
-      });
-      res.json(result);
-    });
 
    
 
@@ -208,7 +201,6 @@ app.get("/appointments", async (req, res) => {
 
   
 
-    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
     );
